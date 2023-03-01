dc93c8b494b158d49a9214e148fc2491222b08cd
Rohan656 <183319@pakaims.edu.pk>
2023-03-01 23:44:50 +0500

    file uploaded successfully

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function add(){
+    const field1 = 2
+    const field2 = 3
+    let result = field1 + field2
+    console.log(result)
+}
+
+function mul(){
+    const field1 = 77
+    const field2 = 9
+    let result = field1 * field2
+    console.log(result)
+}
+
+
+function div(){
+    const field1 = 6
+    const field2 = 9
+    var result = field1 / field2
+    console.log(result)
+}
